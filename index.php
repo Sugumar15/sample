5f234dec1d55f83067c63670019a8ae4b536e81d
Sugumar15 <34616272+Sugumar15@users.noreply.github.com>
2021-06-06 11:06:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
 <?php
-echo "<html>";
-echo "<body>";
+echo "Hello World!";
+?> 
 
-echo "<h1>My first PHP page</h1>";
-
-
-echo "<h2> Hello World!....</h2>";
-
-
-echo "</body>";
-echo "</html>";
-?>
+</body>
+</html>
