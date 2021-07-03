b7cfba8bddb74c9e399332b73895f854c0357026
Sugumar15 <34616272+Sugumar15@users.noreply.github.com>
2021-07-03 23:41:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ may your birthday be filled with laughter.......
 </center>
 </div>
 <marquee width="100%" direction="left" height="25px">
-Once Again Happy BORNDAY to MATHINI,TREAT MUKKIYAM MADAM
+  Once Again Happy BORNDAY to MATHINI,<B><I>TREAT MUKKIYAM MADAM</I></B>
 </marquee>
 </div>
 </body>
