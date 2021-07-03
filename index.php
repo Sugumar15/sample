2d6fb66583cc8389e1a8ff6b8fbaf220b34ad0cb
Sugumar15 <34616272+Sugumar15@users.noreply.github.com>
2021-07-03 23:14:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+?>
+<html>
+<head>
+<style>
+body {
+  background-color: #EEFBFB;
+}
+img {
+  border: 5px solid #555;
+}
+</style>
+</head>
+<body>
+<div >
+<marquee width="100%" direction="right" height="25px">
+Happy BORNDAY to MATHINI
+</marquee>
+</div>
+<div style='background-color: #12232E;'><center>
+<h1 style='color:white;'>
+PAVITHRA[MATHINI] BirthDay 
+<br>celebrations....
+</h1></center></div>
+<div>
+<center>
+ Dear pavitra mathini,<br>
+ Wish you a great happy birthday and may all your dreams come true live long<br>
+ <h1>
+Happy BirthDay PAVI....Have a great year ahead
+</h1>
+<!---<br>
+<h1>
+may this day be as special as you are...
+</h1>
+<br>
+<h1>
+may your birthday be filled with laughter.......
+</h1>-->
+</center>
+</div>.
+<div >
+<center>
+<img src="3.png" alt="Girl in a jacket" width="400" height="400">
+</center>
+</div>
+<div>
+</div>
+<div>
+<center>
+<img src="1.png" alt="Girl in a jacket" width="400" height="400">
+</center>
+</div>
+<div>
+<center>
+<img src="2.png" alt="Girl in a jacket" width="400" height="400">
+</center>
+</div>
+<div>
+<center>
+<img src="4.jpg" alt="Girl in a jacket" width="400" height="500">
+</center>
+</div>
+<div>
+<center>
+<img src="5.jpg" alt="Girl in a jacket" width="400" height="450">
+</center>
+</div>
+<marquee width="100%" direction="left" height="25px">
+Once Again Happy BORNDAY to MATHINI
+</marquee>
+</div>
+</body>
+</html>
