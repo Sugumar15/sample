ec08ea831780747ea7648e95dc8191282112735f
Sugumar15 <34616272+Sugumar15@users.noreply.github.com>
2021-11-03 20:22:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
 <p> 
 <center>
 
-<div style="width:500; height: 2500; background-color: #460eed">
+<div style="width:800; height: 2500; background-color: #460eed">
 <h1 style='color:white;'>
 Happy Diwali to PAVI
 </h1>
 
 <center>
-<img class="center" src="happy-diwali.png" width="300" alt="Happy Diwali greating wishes">
+<img class="center" src="happy-diwali.png" width="500" height="500" alt="Happy Diwali greating wishes">
 </center>
 <h2 style='color:white;'>Wish <br>you<br> Happy<br> Diwali</h2>
 <div></div>
-<img class="center" src="posvanam.gif" width="200" alt="Happy Diwali greating wishes"> 
-<img src="sweet.gif" width="100" alt="Happy Diwali greating wishes">
-<img class="center" src="saram.gif" width="200" alt="Happy Diwali greating wishes">
+<img class="center" src="posvanam.gif" width="400" height="400" alt="Happy Diwali greating wishes"> 
+<img src="sweet.gif" width="400" height="400" alt="Happy Diwali greating wishes">
+<img class="center" src="saram.gif"width="400" height="400" alt="Happy Diwali greating wishes">
 
 </div>
 </center>
